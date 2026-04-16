8028ade3698e7ee872d7f1ae0164712dfac5c845
achmad <faa1909@outlook.com>
2026-04-16 11:36:21 +0700

    chore: update basepath

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/',
+  base: '/',
   plugins: [react(), markdownPlugin({ mode: [Mode.MARKDOWN] })],
   resolve: {
     alias: {
